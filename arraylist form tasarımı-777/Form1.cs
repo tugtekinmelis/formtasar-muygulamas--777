648fa3e05c083d7ef8560a138405513d8c3811f8
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-12-04 10:19:38 +0300

    Add files via upload

---
+++ arraylist form tasarımı-777/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arraylist_form_tasarımı_777
+{
+    public partial class Form1 : Form
+    {
+        ArrayList sehirler = new ArrayList();   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Clear();
+            sehirler.Add(txtSehirAdi.Text);
+        }
+    }
+}
